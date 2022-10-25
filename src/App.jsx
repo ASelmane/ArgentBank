22107374de6710adb1176ae5301ff57fc32098a0
ASelmane <adrien.selmane@gmail.com>
2022-10-25 17:02:37 +0200

    main: login jwt token

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './styles/css/main.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
@@ -15,6 +16,7 @@ const App = () => {
                 <Routes>
                     <Route path="/*" element={<Home />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/profile" element={<Profile />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,70 @@
+import React from "react";
+
 const Login = () => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [error, setError] = React.useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await login(email, password);
+    };
+
+    const login = async (email, password) => {
+            const response = await fetch('http://localhost:3001/api/v1/user/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password : password,
+                }),
+            });
+            const data = await response.json();
+            if (data.status === 200) {
+                await redirectProfile(data.body.token);
+                setError("");
+            }
+            else {
+                setError(data.message);
+            }
+    };
+
+    const redirectProfile = async (token) => {
+        await localStorage.setItem('token', token);
+        window.location.href = '/profile';
+        const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+        const data = await response.json();
+        console.log(data.body);
+    };
+
     return (
         <main className="main bg-dark">
             <section className="sign-in-content">
                 <i className="fa fa-user-circle sign-in-icon"></i>
                 <h1>Sign In</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-wrapper">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" id="username" />
+                        <label htmlFor="email">Username</label>
+                        <input type="text" id="email" onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" />
+                        <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className="input-remember">
-                        <input type="checkbox" id="remember-me" />
+                        <input type="checkbox" id="remember-me"/>
                         <label htmlFor="remember-me">Remember me</label>
                     </div>
                     <button className="sign-in-button">Sign In</button>
+                    <p>{error}</p>
                 </form>
             </section>
         </main>
